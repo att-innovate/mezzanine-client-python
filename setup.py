99a050687c556fa5c894adc4bf9cab3f6f6a5004
George Cushen <gcushen@users.noreply.github.com>
2016-03-26 11:24:00 +0000

    Add PyYAML dependency

--- setup.py
+++ setup.py
@@ -46,7 +46,8 @@ setup(
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'requests>=2.9.1',
-        'requests-oauthlib>=0.6.1'
+        'requests-oauthlib>=0.6.1',
+        'PyYAML>=3.11'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
