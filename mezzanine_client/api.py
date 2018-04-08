3e43c2eee84015a87522024aa16a14bc531b1d71
gcushen <gcushen@users.noreply.github.com>
2018-04-08 10:02:50 +0100

    Refactor

--- mezzanine_client/api.py
+++ mezzanine_client/api.py
@@ -164,16 +164,16 @@ class Mezzanine(MezzanineCore):
         """
         return self._get(['posts', int(item_id)])
 
-    def get_posts(self, offset=0, limit=10, category="", date_min = ""):
+    def get_posts(self, offset=0, limit=10, category_name="", date_min=""):
         """
         Get published blog posts
         :param offset: pagination offset
         :param limit: pagination limit
-        :param category: the category which the post related to
-        :param limit: date_min of posts to return
+        :param category_name: category name
+        :param date_min: minimum date published (e.g. `2018-01-01`)
         :return: list of dicts for most recently published blog posts
         """
-        return self._get(['posts?offset={}&limit={}&category_name={}&date_min={}'.format(int(offset), int(limit), category, date_min)])['results']
+        return self._get(['posts?offset={}&limit={}&category_name={}&date_min={}'.format(int(offset), int(limit), category_name, date_min)])['results']
 
     def create_post(self, data):
         """
